a779aa9d787413b126f370f16b0c22b1701e8bc5
KENNETH PHANG <bunnyppl@hotmail.com>
2023-02-15 10:06:06 +0800

    upd

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
@@ -11,6 +11,9 @@ import { UploadComponent } from './components/upload.component';
 
 import { WebcamModule } from 'ngx-webcam';
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -24,6 +27,14 @@ import { AppRoutingModule } from './app-routing.module';
     HttpClientModule,
     ReactiveFormsModule,
     WebcamModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
   ],
   providers: [ CameraService ],
   bootstrap: [AppComponent]
--- server/src/main/java/sg/edu/nus/iss/fileupload/server/controller/FileUploadController.java
+++ server/src/main/java/sg/edu/nus/iss/fileupload/server/controller/FileUploadController.java
@@ -2,6 +2,8 @@ package sg.edu.nus.iss.fileupload.server.controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Base64;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -9,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,6 +20,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import jakarta.json.Json;
 import jakarta.json.JsonObject;
+import sg.edu.nus.iss.fileupload.server.model.Post;
 import sg.edu.nus.iss.fileupload.server.services.FileUploadService;
 import sg.edu.nus.iss.fileupload.server.services.S3Service;
 
@@ -27,7 +32,8 @@ public class FileUploadController {
 
     @Autowired
     private FileUploadService flSvc;
-
+    private static final String BASE64_PREFIX_DECODER = "data:image/png;base64,";
+        
     @PostMapping(path="/upload-ng", 
             consumes=MediaType.MULTIPART_FORM_DATA_VALUE, 
             produces=MediaType.APPLICATION_JSON_VALUE)
@@ -70,5 +76,16 @@ public class FileUploadController {
         model.addAttribute("key", key);
         
         return "upload";
+    }
+
+    @GetMapping(path="/get-image/{postId}")
+    public String retrieveImage(@PathVariable Integer postId, Model model){
+        Optional<Post> opt= flSvc.getPostById(postId);
+        Post p = opt.get();
+        String encodedString = Base64.getEncoder().encodeToString(p.getImage());
+        model.addAttribute("title", p.getTitle());
+        model.addAttribute("complain", p.getComplain());
+        model.addAttribute("file", BASE64_PREFIX_DECODER + encodedString);
+        return "blob";
     }
 }
